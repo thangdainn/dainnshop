c7460ff53885b80858b88af63ee2242473c1bd44
Blahblah2003 <tanvantran111@gmail.com>
2024-05-05 18:38:31 +0700

    Reformat admin to MVC. Remove Add pages from sidebar. Changed add,update,delete products to have 3 images.

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -22,14 +22,14 @@
         </footer>
     </main>
 
-    <script src="assets/js/jquery-3.7.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/smooth-scrollbar.min.js"></script>
+    <script src="../assets/js/jquery-3.7.1.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/perfect-scrollbar.min.js"></script>
+    <script src="../assets/js/smooth-scrollbar.min.js"></script>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-    <script src="assets/js/custom.js"></script>
+    <script src="../assets/js/custom.js"></script>
 
     <!-- Alertify Js -->
     <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/alertify.min.js"></script>
